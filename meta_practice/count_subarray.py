1ed424c8d1e232d49c1a3426bf9ce955555d096d
snagas22 <snagas@umich.edu>
2022-02-28 01:35:46 -0500

    more efficient count subarray solution

--- meta_practice/count_subarray.py
+++ meta_practice/count_subarray.py
@@ -29,6 +29,28 @@ def count_subarrays(arr):
   return ret
 
 
+# more efficient
+def count_subarrays(arr):
+  n = len(arr)
+  res = [1] * n
+  stack = [-1]
+  #left
+  for i in range(n):
+    while len(stack) > 1 and arr[stack[-1]] < arr[i]:
+      stack.pop()
+    res[i] += i - stack[-1] - 1
+    stack.append(i)
+
+  # from right
+  stack = [n]
+  for i in range(n - 1, -1, -1):
+    while len(stack) > 1 and arr[stack[-1]] < arr[i]:
+      stack.pop()
+    res[i] += stack[-1] - i - 1
+    stack.append(i)
+  return res
+
+
 # These are the tests we use to determine if the solution is correct.
 # You can add your own at the bottom.
 
